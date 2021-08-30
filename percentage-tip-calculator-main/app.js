4132d3c458d1a9a316cde3db01cf015fefb6248d
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2021-08-30 21:48:32 +0530

    Add files via upload

---
+++ percentage-tip-calculator-main/app.js
@@ -0,0 +1,7 @@
+let btn=document.getElementById('calculate').addEventListener('click',()=>{
+    let data=document.getElementById('input').value;
+    let tip=document.getElementById('tip').value;
+    let result=(data-(data-(data*tip/100))).toFixed(2);
+   alert(`The Tip: ${result}`);
+
+});
