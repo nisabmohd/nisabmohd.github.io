4a7e3f085b9791eba0a7b843b52bdf463d3d6c6d
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2021-09-02 22:59:08 +0530

    Add files via upload

---
+++ dark-mode-toggle/script.js
@@ -0,0 +1,30 @@
+var c=0;
+let btn=document.getElementById('icon').addEventListener('click',()=>{
+   if(c==0){
+    
+       dark();
+
+   }
+   else if(c==1){
+   
+       light();
+   }
+});
+function dark(){
+    c=1;
+    document.getElementById('icon').innerHTML=`<img id="btn" src="download.png" alt="light">`;
+    document.getElementById('ctn').style.background=`rgb(22, 22, 22)`;
+    for(let i=0;i<16;i++){
+    note=document.querySelectorAll('.grid1')[i];
+    note.style.color=`white`;
+    }
+}
+function light(){
+    c=0;
+    document.getElementById('icon').innerHTML=`<img id="btn" src="dark-mode.png" alt="dark">`;
+    document.getElementById('ctn').style.background=`white`;
+    for(let i=0;i<16;i++){
+    note=document.querySelectorAll('.grid1')[i];
+    note.style.color=`black`;
+    }
+}
