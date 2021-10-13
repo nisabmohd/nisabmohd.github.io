a8e4f46dff68477d7cec205f41d04306bbc23ed7
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2021-10-13 18:12:24 +0530

    Add files via upload

---
+++ portfolio-website/script.js
@@ -0,0 +1,6 @@
+let contact=document.getElementById('contact').addEventListener('click',()=>{
+    window.scrollTo(0,document.body.scrollHeight);
+})
+let topbtn=document.getElementById('topbtn').addEventListener('click',()=>{
+    window.scrollTo(0,0);
+})
